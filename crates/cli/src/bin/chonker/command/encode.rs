bb8c7ceb9f0f65f7e6124ee90e591866ccd74dca
silvanshade <silvanshade@users.noreply.github.com>
2023-12-22 19:40:08 -0700

    WIP

--- crates/cli/src/bin/chonker/command/encode.rs
+++ crates/cli/src/bin/chonker/command/encode.rs
@@ -6,7 +6,7 @@ pub(crate) async fn encode(path: &Path) -> crate::BoxResult<()> {
     let reader = chonker::io::fast_seq_open(path).await?;
     let reader_size = reader.metadata().await?.len();
     let context = EncodeContext::new(reader_size, true)?.into();
-    let writer = &mut tokio::fs::File::create(path.with_extension("chonker")).await?;
+    let writer = &mut tokio::fs::File::create(path.with_extension("chonk")).await?;
     chonker::ChonkerArchive::encode(context, reader, Some(reader_size), writer).await?;
     Ok(())
 }
